fbe498973bffa6570e7f906ef60d8d763ddcfc0a
fritz duchardt <fritz@duchardt.net>
2021-11-20 11:51:08 +0100

    Renamed switch-context to switch-config

--- config_rc.sh
+++ config_rc.sh
@@ -1,6 +1,6 @@
 current_dir=$(dirname "$0")
 # aliases
 alias {port-forward,pf}="$current_dir"/port-forward/launch_port_forward.sh
-alias {switch-context,sc}=". $current_dir"/switch-context/switch_context.sh
-alias {add-context,ac}="$current_dir"/switch-context/add_context.sh
-alias {remove-context,rc}="$current_dir"/switch-context/remove_context.sh
+alias {switch-config,sc}=". $current_dir"/switch-config/switch_config.sh
+alias {add-config,ac}="$current_dir"/switch-config/add_config.sh
+alias {remove-config,rc}="$current_dir"/switch-config/remove_config.sh
