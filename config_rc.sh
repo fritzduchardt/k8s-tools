123b7ee391fb0de4185cedbf221cd8ec32230fe7
fritz duchardt <fritz@duchardt.net>
2022-01-03 15:11:21 +0100

    Added script to read arbitrary entries from secrets

--- config_rc.sh
+++ config_rc.sh
@@ -5,4 +5,5 @@ alias {add-port-forward,ap}="$current_dir"/port-forward/add_port_forward.sh
 alias {switch-config,sc}=". $current_dir"/switch-config/switch_config.sh
 alias {add-config,ac}="$current_dir"/switch-config/add_config.sh
 alias {remove-config,rc}="$current_dir"/switch-config/remove_config.sh
+alias {read-secret,rs}="$current_dir"/read-secret/read_secret.sh
 alias read-cert="$current_dir"/read-cert/read_cert.sh
---
+++ read-secret/read_secret.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+usage() {
+  echo "Usage: $(basename "$0") SECRET_NAME NAMESPACE_NAME FIELD_NAME"
+  echo
+  echo "Reads field from secret and base64 decodes it."
+}
+
+check_prerequisites() {
+  local rc=0
+  if ! command -v kubectl >/dev/null
+  then
+      echo "required command kubectl not found!" >&2
+      rc=1
+  fi
+  if ! command -v base64 >/dev/null
+  then
+      echo "required command base64 not found!" >&2
+      rc=1
+  fi
+  return "$rc"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+
+  # parameter validation
+  if [[ ! "$#" -eq 3 ]]
+  then
+    usage >&2
+    exit 2
+  fi
+
+  # prerequisite validation
+  RC=0
+  check_prerequisites
+  RC="$?"
+  if [[ $RC != 0 ]]
+  then
+    echo "prerequisites not met"
+    exit 1
+  fi
+
+  secret="$1"
+  namespace="$2"
+  field=$(sed 's#\.#\\.#g' <<<"$3")
+
+  kubectl get secret -n "$namespace" "$secret" -o jsonpath="{.data.$field}" | base64 -d
+fi
