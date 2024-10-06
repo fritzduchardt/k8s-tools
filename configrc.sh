4495d244d3eb556e8217dc105fbcd811f4975feb
Fritz Duchardt <fritz@duchardt.net>
2024-10-06 19:57:48 +0200

    fix: refactored config rc

--- configrc.sh
+++ configrc.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
-k8s_tools_scripts_dir="$(dirname "$0")"
+script_dir="$(dirname "$0")"
+
+lib::exec_k8s_tool() {
+  local dir="$1" script="$2"
+  shift 2
+  (cd "$dir" && ./"$script" "$@")
+}
 
 # namespace
-exec_clean_namespace() { (cd "$k8s_tools_scripts_dir"/clean-namespace && ./clean_namespace.sh "$@") }
-alias {clean-namespace,cn}="exec_clean_namespace"
+alias {clean-namespace,cn}="lib::exec_k8s_tool $script_dir/clean-namespace clean_namespace.sh"
 
 # ports
-exec_port_forward() { (cd "$k8s_tools_scripts_dir"/port-forward && ./launch_port_forward.sh "$@") }
-alias {port-forward,pf}="exec_port_forward"
-exec_add_port_forward() { (cd "$k8s_tools_scripts_dir"/port-forward && ./add_port_forward.sh "$@") }
-alias {add-port-forward,ap}="exec_add_port_forward"
+alias {port-forward,pf}="lib::exec_k8s_tool $script_dir/port-forward launch_port_forward.sh"
+alias {add-port-forward,ap}="lib::exec_k8s_tool $script_dir/port-forward add_port_forward.sh"
 
 # secrets
-exec_read_secret() { (cd "$k8s_tools_scripts_dir"/secrets && ./read_secret.sh "$@") }
-alias {read-secret,rs}="exec_read_secret"
-exec_write_docker_secret() { (cd "$k8s_tools_scripts_dir"/secrets && ./write_docker_secret.sh "$@") }
-alias {write-docker-secret,wds}="exec_write_docker_secret"
+alias {read-secret,rs}="lib::exec_k8s_tool $script_dir/secrets read_secret.sh"
+alias {write-docker-secret,wds}="lib::exec_k8s_tool $script_dir/secrets write_docker_secret.sh"
 
 # certs
-exec_read_cert() { (cd "$k8s_tools_scripts_dir"/read-cert && ./read_cert.sh "$@") }
-alias cert="exec_read_cert"
-alias {cluster-info,ci}="(cd $k8s_tools_scripts_dir/cluster-info && ./cluster_info.sh)"
+alias cert="lib::exec_k8s_tool $script_dir/certificates read_cert.sh"
+
+# cluster info
+alias {cluster-info,ci}="lib::exec_k8s_tool $script_dir/cluster-info cluster_info.sh"
