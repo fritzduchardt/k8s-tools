cc49204075321da2608ecaa8a1f0ce422cdd5daa
Fritz Duchardt <fritz@duchardt.net>
2025-02-24 12:22:16 +0100

    Corrected cluster info script

--- cluster-info/cluster_info.sh
+++ cluster-info/cluster_info.sh
@@ -4,7 +4,7 @@ source "../lib/log.sh"
 source "../lib/utils.sh"
 
 NS="$(k8s::current_namespace)"
-CONTEXT=$(k8s::current_namespace)
+CONTEXT=$(k8s::current_context)
 if [[ "$NS" != "none" || "$CONTEXT" != "none" ]]; then
   printf "%s // %s" "$NS" "$CONTEXT"
 fi
