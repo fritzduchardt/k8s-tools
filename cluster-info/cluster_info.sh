8fbf2ed04fae2f8b7fc1b661d1d2f4d47e4ca9b9
Fritz Duchardt <fritz@duchardt.net>
2025-03-16 10:21:28 +0100

    Add default value for cluster-info

--- cluster-info/cluster_info.sh
+++ cluster-info/cluster_info.sh
@@ -4,7 +4,8 @@ source "../lib/log.sh"
 source "../lib/utils.sh"
 
 NS="$(k8s::current_namespace)"
+NS="${NS:-none}"
 CONTEXT=$(k8s::current_context)
-if [[ "$NS" != "none" || "$CONTEXT" != "none" ]]; then
+if [[ -n "$NS" || -n "$CONTEXT" ]]; then
   printf "%s // %s" "$NS" "$CONTEXT"
 fi
